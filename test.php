1da660225ef5a6914b9f2009e2fa452a5385d059
YanzYan <18207302862@163.com>
2017-04-24 15:32:49 +0800

    php测试

---
+++ test.php
@@ -0,0 +1,2 @@
+header('Access-Control-Allow-Origin:*');
+{ "sites": [ { "Name": "²ËÄñ½Ì³Ì", "Url": "www.runoob.com", "Country": "CN" }, { "Name": "Google", "Url": "www.google.com", "Country": "USA" }, { "Name": "Facebook", "Url": "www.facebook.com", "Country": "USA" }, { "Name": "Î¢²©", "Url": "www.weibo.com", "Country": "CN" } ] }
